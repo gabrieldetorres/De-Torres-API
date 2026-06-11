b7fb0031e92964476351e2152bf5a48c701d8de4
gabrieldetorres <gabrieldetorres21@gmail.com>
2026-06-11 19:00:24 +0800

    API SWAGGER

--- TimeManagementAPI/obj/Debug/net8.0/TimeManagementAPI.AssemblyInfo.cs
+++ TimeManagementAPI/obj/Debug/net8.0/TimeManagementAPI.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("TimeManagementAPI")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+9c2648d59b0bff0ef3ce3dba7ac6cb3a3921f4c6")]
 [assembly: System.Reflection.AssemblyProductAttribute("TimeManagementAPI")]
 [assembly: System.Reflection.AssemblyTitleAttribute("TimeManagementAPI")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
